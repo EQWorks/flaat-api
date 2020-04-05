815d72762bbbc5065a59c142a860868325475a4f
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-05 17:02:53 -0400

    api - enable cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const config = require('../config')
 const { Pool } = require('pg')
 
@@ -8,6 +9,7 @@ const register = require('./routes/register')
 const { STAGE = 'local', API_VER = 'local' } = process.env
 const db = new Pool(config.db)
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
