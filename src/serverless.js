937d4ed4c33dca0a2458737e45ed499f41ee0817
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-05 17:27:32 -0400

    devops - serverless mount to stage

--- src/serverless.js
+++ src/serverless.js
@@ -1,6 +1,13 @@
 const serverless = require('serverless-http')
+const express = require('express')
 
-const app = require('./app')
+const root = require('./app')
+
+
+const app = express()
+const { STAGE = 'local', API_VER = 'local' } = process.env
+app.get(`/${STAGE}`, (_, res) => res.json({ STAGE, API_VER }))
+app.use(`/${STAGE}`, root)
 
 
 module.exports.handler = serverless(app)
