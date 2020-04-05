d9b25f509cbd854c2f76c59f171742b29eb21841
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-05 17:58:57 -0400

    api - mount cors in serverless.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const config = require('../config')
 const { Pool } = require('pg')
 
@@ -8,7 +7,6 @@ const register = require('./routes/register')
 
 const db = new Pool(config.db)
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- src/serverless.js
+++ src/serverless.js
@@ -1,11 +1,13 @@
 const serverless = require('serverless-http')
 const express = require('express')
+const cors = require('cors')
 
 const root = require('./app')
 
 
-const app = express()
 const { STAGE = 'local', API_VER = 'local' } = process.env
+const app = express()
+app.use(cors())
 app.get(`/${STAGE}`, (_, res) => res.json({ STAGE, API_VER }))
 app.use(`/${STAGE}`, root)
 
