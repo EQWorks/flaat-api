98480cc484836ed1827b1e556df3fcb9e324dfcd
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-05 16:18:18 -0400

    devops - serverless definition for deployment

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const { Pool } = require('pg')
 const register = require('./routes/register')
 
 
+const { STAGE = 'local', API_VER = 'local' } = process.env
 const db = new Pool(config.db)
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (_, res) => res.json({ STAGE, API_VER }))
 app.use('/register', register(db))
 
 module.exports = app
